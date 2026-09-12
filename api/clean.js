acb48a4941f19d717fb896757bf98d1697236322
DRoberts968 <robertssams968@gmail.com>
2026-09-13 00:57:18 +0100

    Fix: Remove unsupported runtime config from serverless function

--- api/clean.js
+++ api/clean.js
@@ -1,7 +1,3 @@
-export const config = {
-  runtime: 'nodejs',
-};
-
 export default async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Credentials', 'true');
