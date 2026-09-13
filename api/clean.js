730e0223ebfa14c0106287496cf8b679b9b16aed
DRoberts968 <robertssams968@gmail.com>
2026-09-13 01:44:48 +0100

    Convert API to CommonJS format for Vercel compatibility

--- api/clean.js
+++ api/clean.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   // CORS headers
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -65,4 +65,4 @@ export default async function handler(req, res) {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
